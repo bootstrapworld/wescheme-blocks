1a0331fa157086a07f462144b29a299a585db0cc
Paul Carduner <paul@carduner.net>
2021-07-06 15:34:52 -0700

    Make the webpack bundling config work

--- webpack.config.js
+++ webpack.config.js
@@ -4,44 +4,45 @@ const {
   getWebpackBundleConfig,
 } = require('codemirror-blocks/lib/toolkit/webpack');
 
-const config = getWebpackDevServerConfig({
+const devServerConfig = getWebpackDevServerConfig({
   context: path.resolve('dev-server'),
   entry: './index.js',
 });
+const bundleConfig = getWebpackBundleConfig({
+  entry: {
+    CodeMirrorBlocks: path.resolve(__dirname, 'src', 'languages', 'wescheme', 'index'),
+  },
+});
 
-const devServerConfig = {
-  ...config,
-  module: {
-    ...config.module,
-    rules: [
-      ...config.module.rules,
-      // Add loader for .rkt files
-      {
-        test: /\.rkt$/,
-        use: [{ loader: 'raw-loader' }],
-      },
-    ],
-  },
-  resolve: {
-    ...config.resolve,
-    alias: {
-      // Add aliases needed by WeschemeParser.js
-      // TODO(pcardune): stop using aliases and just import
-      // directly from the right place...?
-      ...config.resolve?.alias,
-      jsnums: 'wescheme-js/src/runtime/js-numbers',
-      lex: 'wescheme-js/src/lex',
-      types: 'wescheme-js/src/runtime/types',
-      structs: 'wescheme-js/src/structures',
-    },
-  },
+const alias = {
+  jsnums: 'wescheme-js/src/runtime/js-numbers',
+  lex: 'wescheme-js/src/lex',
+  types: 'wescheme-js/src/runtime/types',
+  structs: 'wescheme-js/src/structures',
+};
+
+devServerConfig.module.rules.push({
+  test: /\.rkt$/,
+  use: [{ loader: 'raw-loader' }],
+});
+
+// Add aliases needed by WeschemeParser.js
+// TODO(pcardune): stop using aliases and just import
+// directly from the right place...?
+devServerConfig.resolve.alias = alias;
+bundleConfig.resolve.alias = alias;
+
+bundleConfig.externals = {
+  'codemirror': 'codemirror',
+  'codemirror/addon/search/search' : 'codemirror',
+  'codemirror/addon/search/searchcursor' : 'codemirror',
+  'jsnums': 'jsnums',
+  'lex': 'plt.compiler',
+  'types': 'types',
+  'structs': 'plt.compiler',
 };
 
 module.exports = [
   devServerConfig,
-  getWebpackBundleConfig({
-    entry: {
-      CodeMirrorBlocks: path.resolve(__dirname, 'src', 'index'),
-    },
-  }),
+  bundleConfig,
 ];
